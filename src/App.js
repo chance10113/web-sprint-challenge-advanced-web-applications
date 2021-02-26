54c90ceb94e63a18f8c32e5db874040a33ccf1bd
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-02-26 13:21:25 -0600

    Added Private  Route

--- src/App.js
+++ src/App.js
@@ -3,20 +3,18 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
+import PrivateRoute from "./components/PrivateRoute";
+import BubblePage from "./components/BubblePage";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
+        <PrivateRoute exact path="/protected" component={BubblePage} />
       </div>
     </Router>
   );
 }
 
 export default App;
-
-//Task List:
-//1. Render BubblePage as a PrivateRoute
-
-// WANTS US to use localHistory/Authorization with Private Route
