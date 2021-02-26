11a5caa84853ceef5acd0a6c4439fdce88827fb6
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-02-26 15:47:54 -0600

    Finished MVP

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,15 +1,102 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import BubblePage from "./BubblePage";
+import ColorList from "./ColorList";
+import Bubbles from "./Bubbles";
 
-test("Renders BubblePage without errors", () => {
-  // Finish this test
+const mockData = [
+  {
+    color: "aliceblue",
+    code: {
+      hex: "#f0f8ff",
+    },
+    id: 1,
+  },
+  {
+    color: "limegreen",
+    code: {
+      hex: "#99ddbc",
+    },
+    id: 2,
+  },
+  {
+    color: "aqua",
+    code: {
+      hex: "#00ffff",
+    },
+    id: 3,
+  },
+  {
+    color: "aquamarine",
+    code: {
+      hex: "#7fffd4",
+    },
+    id: 4,
+  },
+  {
+    color: "lilac",
+    code: {
+      hex: "#9a99dd",
+    },
+    id: 5,
+  },
+  {
+    color: "softpink",
+    code: {
+      hex: "#dd99ba",
+    },
+    id: 6,
+  },
+  {
+    color: "bisque",
+    code: {
+      hex: "#dd9a99",
+    },
+    id: 7,
+  },
+  {
+    color: "softyellow",
+    code: {
+      hex: "#dcdd99",
+    },
+    id: 8,
+  },
+  {
+    color: "blanchedalmond",
+    code: {
+      hex: "#ffebcd",
+    },
+    id: 9,
+  },
+  {
+    color: "blue",
+    code: {
+      hex: "#6093ca",
+    },
+    id: 10,
+  },
+  {
+    color: "blueviolet",
+    code: {
+      hex: "#8a2be2",
+    },
+    id: 11,
+  },
+];
+
+test("Renders BubblePage without errors", async () => {
+  render(<BubblePage />);
 });
 
-test("Fetches data and renders the bubbles on mounting", () => {
-  // Finish this test
-});
+test("Fetches data and renders the bubbles on mounting", async () => {
+  render(<ColorList colors={mockData} />);
+  render(<Bubbles colors={mockData} />);
 
-//Task List
-//1. Setup test for basic rendering of component
-//2. Setup test for initial rendering of bubbles on loading
+  const color = await screen.findByText(/aqua/i);
+  const secondColor = await screen.findByText(/blanchedalmond/i);
+  const bubblesOnScreen = await screen.findByTestId(/bubbletest/i);
+
+  expect(color).toBeInTheDocument();
+  expect(secondColor).toBeInTheDocument();
+  expect(bubblesOnScreen).toBeInTheDocument();
+});
